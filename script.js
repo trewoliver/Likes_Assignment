909ae7bd997319febcd55a5356e7445982610c06
John Doe <johndoe@example.com>
2023-05-21 22:31:19 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function Likes1(click) {
+    const Likes = document.getElementById('Likes');
+    const sumvalue=parseInt(Likes.innerText) + click;
+    console.log(sumvalue + click);
+    Likes.innerText = sumvalue;
+};
+
+function Likes2(click) {
+    const Likes2 = document.getElementById('Likes2');
+    const sumvalue=parseInt(Likes2.innerText) + click;
+    console.log(sumvalue + click);
+    Likes2.innerText = sumvalue;
+};
+
+function Likes3(click) {
+    const Likes3 = document.getElementById('Likes3');
+    const sumvalue=parseInt(Likes3.innerText) + click;
+    console.log(sumvalue + click);
+    Likes3.innerText = sumvalue;
+};
+
